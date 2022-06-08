0f91cd0366091c9f396804c3c139221991fe1faf
Asper <asper@strategicsale.com>
2022-06-08 13:52:57 +0800

    done

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npx wrangler dev src/index.js` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npx wrangler publish src/index.js --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import BotRate from './bot_rate';
 
 export default {
-  async fetch(request) {
-    return new Response("Hello World!");
+  async fetch(request, env) {
+    var url = new URL(request.url);
+
+    if (url.pathname === '/') {
+      let botRate = await BotRate();
+
+      const json = JSON.stringify(botRate, null, 2);
+
+      return new Response(json, {
+        headers: {
+          'content-type': 'application/json;charset=UTF-8',
+        },
+      });
+    }
+
+    return new Response(null, {
+      status: 404,
+      statusText: 'Not Found',
+    });
   },
 };
